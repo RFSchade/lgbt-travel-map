0ac6a939fc4628ab35fa348d2388a3731435df1a
Sophia Kleist Karlson <sophia.karlson@gmail.com>
2021-05-30 09:07:39 +0200

    finally got further with the find me feature in shiny

--- scripts/shinyapp/app.R
+++ scripts/shinyapp/app.R
@@ -1,43 +1,20 @@
 
-pacman::p_load(pacman, shiny, leaflet, tidyverse)
+pacman::p_load(pacman, shiny, leaflet, leatlet.extras, tidyverse)
 
-#https://www.rdocumentation.org/packages/leaflet/versions/2.0.4.1/topics/leafletOutput
-#UI is the user interface. the "map" is the outputId, which is the "output variable to read from"
-ui <- fluidPage(titlePanel("LGBTQ+ Travel Map"), leafletOutput("map")) 
+ui <- fluidPage(
+  titlePanel("LGBTQ+ Travel Map"), 
+  leafletOutput("map"))
 
 
 server <- function(input, output, session) {
   
-  #center_coor <- c()
-  
-  # get the coordinates from the center and print them
-  observeEvent(input$map_bounds, {
-    event <- input$map_bounds
-    
-    lat <- mean(event$north, event$south)
-    lon <- mean(event$west, event$east)
-    
-    center_coor <- c(lat, lon) # should it be lon, lat, or lat, lon??
-    print(paste0("map center - lat: ", lat, ", lon: ", lon))
-    #print(center_coor)
-    print(center_coor)
-    
-    return(center_coor)
-  })
-  
-  
-  
-  
-  # map
   output$map <- renderLeaflet({
+    
     leaflet() %>%
-      addTiles() %>% # insert this in the parenthesis: attribution = 'By Rebecca Schade and Sophia Karlson'
-      addControl("Queer Travel Map", position = "topleft", className="map-title") %>% # make this bigger!
+      addTiles() %>%
+      addControl("Queer Travel Map", position = "topleft", className="map-title") %>% #
       addProviderTiles("Esri.WorldTopoMap", group = "Topographic") %>%
       addProviderTiles("Esri.WorldImagery", group = "Aerial") %>%
-      
-      # add center marker
-      #addMarkers(data = center_coor) %>% #lng = lon, lat = lat) %>% 
       
       addCircleMarkers(data = safespace_crs,
                        opacity = 0.5, color = "black", stroke = TRUE,
@@ -46,46 +23,24 @@ server <- function(input, output, session) {
                                       "<br> Type: ", test_data$amenity,
                                       "<br> Website: ", test_data$website,
                                       "<br> Opening hours: ", test_data$opnng_h), #,
-                       # if we had the data, these could be added as well:
-                       #"<br> Address: ", test_data$address),
-                       # who it is for (only men, everyone etc)
-                       group = "Safe spaces",#if we don't want the user to be able to not see all the safe spaces, cut this
-                       #) %>%
+                       group = "Safe spaces",
+                       
                        clusterOptions = markerClusterOptions()) %>%
       
-      # add density overlay here - e.g. polygons like here:
-      addPolygons(data = safe_ch,
-                  fill = T, weight = 2, color = "purple",
-                  popup = paste0("Name: ", "gayboorhood-name"), 
-                  group = "Safe space density") %>%
       
-      # do the control stuff
-      addLayersControl(
-        baseGroups = c("Topographic","Aerial"),
-        overlayGroups = c("Safe spaces", "Safe space density"),
-        options = layersControlOptions(collapsed = T)) %>% 
-      
-      #leaflet() %>%
-      # addProviderTiles(providers$Stamen.TonerLite,
-      #                 options = providerTileOptions(noWrap = TRUE)) %>%
-      
-      addEasyButton(
-        easyButton(
-          position = "topleft",
-          icon = "fa-crosshairs",
-          title = "Locate Me",
-          onClick = JS(
-            c(
-              "function(btn,  map){map.locate({setView:true,enableHighAccuracy: true })}"
-            )
-          )
-        )
-      ) %>% 
-      
-      addMarkers(
-        lng = center_coor[1], 
-        lat = center_coor[1],
-        popup = paste("You are here!")) %>% 
+      #addEasyButton(
+       # easyButton(
+        #  position = "topleft",
+         # icon = "fa-crosshairs",
+          #title = "Locate Me",
+          #onClick = JS("
+           # function(btn, map) {
+            #  Shiny.onInputChange('my_easy_button', 'clicked');
+            #}"
+                       
+          #)
+        #)
+      #) %>% 
       
       addMeasure(
         position = "topleft",
@@ -93,8 +48,37 @@ server <- function(input, output, session) {
         primaryAreaUnit = "sqmeters",
         activeColor = "#3D535D",
         completedColor = "#7D4479",
-        localization = "en")
+        localization = "en") %>% 
+      
+      addLayersControl(
+        baseGroups = c("Topographic","Aerial"),
+        overlayGroups = c("Safe spaces", "Gayborhoods", "My location"),
+        options = layersControlOptions(collapsed = T)) %>% 
+      
+      addControlGPS(
+        options = gpsOptions(position = "topleft", 
+                             activate = TRUE, 
+                             autoCenter = TRUE, maxZoom = 10, 
+                             setView = TRUE))
+  })
+  
+  observe(
+    print(paste0("map center - lat: ", input$map_gps_located$coordinates$lat, ", lon: ", input$map_gps_located$coordinates$lng))
+  )
+  
+  observe({
     
+    event <- input$map_gps_located
+    if (is.null(event))
+      return()
+    
+    leafletProxy("map") %>% clearPopups() %>% 
+      #addMarkers(
+       # lng = event$coordinates$lng,
+        #lat = event$coordinates$lat) %>% 
+      addPopups(
+        lng = event$coordinates$lng,
+        lat = event$coordinates$lat, "Here are you")
   })
   
 }
